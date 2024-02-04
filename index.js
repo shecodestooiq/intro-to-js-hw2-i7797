f468c9199800a4b96b26fc3106d455f7d8683331
nura <nooraliabdalhossen@gmail.com>
2024-02-04 23:34:22 +0300

    done hw2

---
+++ index.js
@@ -0,0 +1,18 @@
+var a=10;
+var b=5;
+
+
+console.log( 'a:',a ,'b:', b)
+
+console.log('sum :',a+b)
+console.log('division:',a/b)
+console.log('multip:',a*b)
+console.log('subs:',a-b)
+console.log('a is greater than b:',a>b)
+console.log('a is less than b :',a<b)
+console.log('a is eaqual b:',a==b)
+console.log(' a greater or equal b:',a>=b)
+console.log(' a less or equal b:',a<=b)
+console.log('And :',a>b &&b==5)
+console.log('Or :',a>b || b==5)
+console.log('Not :',!(a=10))
